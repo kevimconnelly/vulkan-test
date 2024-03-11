7fd512d3645f2688866cb097b77f069077f0d435
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-03-10 20:32:02 -0700

    create vk instance

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define GLFW_NICLUDE_VULKAN
+#define GLFW_INCLUDE_VULKAN
 
 #include<GLFW/glfw3.h>
 #include <iostream>
@@ -20,6 +20,8 @@ public:
 private:
     GLFWwindow* window;
 
+    VkInstance instance;
+
     void initWindow() {
         glfwInit();
 
@@ -30,7 +32,7 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -44,6 +46,34 @@ private:
 
         glfwTerminate();
     }
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    };
 };
 
 int main() {
