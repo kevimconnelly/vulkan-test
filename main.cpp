a9cdaca5196cff80b539fd1669e58e84c9bd7115
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-04-03 20:06:37 -0700

    update viewport state

--- main.cpp
+++ main.cpp
@@ -382,7 +382,9 @@ private:
         VkPipelineViewportStateCreateInfo viewportState{};
         viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
         viewportState.viewportCount = 1;
+        viewportState.pViewports = &viewport;
         viewportState.scissorCount = 1;
+        viewportState.pScissors = &scissor;
 
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
