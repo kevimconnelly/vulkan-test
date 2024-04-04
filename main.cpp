7a0211911ed491a1075ea279571454289245f55e
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-04-03 20:07:16 -0700

    revert viewport state

--- main.cpp
+++ main.cpp
@@ -382,9 +382,7 @@ private:
         VkPipelineViewportStateCreateInfo viewportState{};
         viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
         viewportState.viewportCount = 1;
-        viewportState.pViewports = &viewport;
         viewportState.scissorCount = 1;
-        viewportState.pScissors = &scissor;
 
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
