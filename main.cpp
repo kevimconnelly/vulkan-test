204e912b4c4456c4d4ad10b519467c72225ce9e4
Kevin Connelly <kevinconnelly1337@gmail.com>
2024-04-06 10:30:47 -0700

    create color attachment struct

--- main.cpp
+++ main.cpp
@@ -363,6 +363,10 @@ private:
         colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
         colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
         colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference colorAttachmentRef{};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
     }
 
     void createGraphicsPipeline() {
